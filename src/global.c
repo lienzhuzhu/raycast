7d5a68638bb19efa867624631bcb46622503e662
Lien Zhu <lienzhucs@gmail.com>
2024-02-07 20:44:05 -0800

    removes corner debug squares

--- src/global.c
+++ src/global.c
@@ -10,17 +10,6 @@
 
 void draw_guides(void)
 {
-    Vector2 size = {10.f, 10.f};
-    Vector2 topLeft = {10.f, 10.f};
-    Vector2 topRight = {(GetScreenWidth()) - 20.f, 10.f};
-    Vector2 bottomLeft = {10.f, (GetScreenHeight()) - 20.f};
-    Vector2 bottomRight = {(GetScreenWidth()) - 20.f, (GetScreenHeight()) - 20.f};
-
-    DrawRectangleV(topLeft, size, RED);
-    DrawRectangleV(topRight, size, BLUE);
-    DrawRectangleV(bottomLeft, size, BLUE);
-    DrawRectangleV(bottomRight, size, RED);
-
     Vector2 points[4] = {
         {SCREEN_CENTER_X - SCREEN_CENTER_X / 2.f, SCREEN_CENTER_Y - SCREEN_CENTER_Y / 2.f},
         {SCREEN_CENTER_X + SCREEN_CENTER_X / 2.f, SCREEN_CENTER_Y - SCREEN_CENTER_Y / 2.f},
